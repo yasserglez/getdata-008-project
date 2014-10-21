38c564d63d0566355c3ddbd2a289d95adc0f6d1f
Yasser Gonzalez Fernandez <contact@yglezfdez.com>
2014-10-21 13:09:09 -0400

    Record the download date in retrieve_raw_data

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,14 @@
 
 # Functions performing each part of the analysis:
 
-retrieve_raw_data <- function (raw_data_dir, force = FALSE) {
+retrieve_raw_data <- function(raw_data_dir, force = FALSE) {
     data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     data_destfile <- "UCI HAR Dataset.zip"
     if (force || !file.exists(data_destfile)) {
         download.file(data_url, data_destfile, method = "curl")
+        download_date <- Sys.time()
+    } else {
+        download_date <- file.info(data_destfile)[, "ctime"]
     }
     if (force || !file.exists(raw_data_dir)) {
         unzip(data_destfile, overwrite = TRUE)
@@ -14,6 +17,7 @@ retrieve_raw_data <- function (raw_data_dir, force = FALSE) {
             file.rename(orig_raw_data_dir, raw_data_dir)
         }
     }
+    return(download_date)
 }
 
 
