e2ca529c703237701514c24a1da9501ef17f0544
Yasser Gonzalez Fernandez <contact@yglezfdez.com>
2014-10-21 12:58:44 -0400

    Implement retrieve_raw_data in run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,24 @@
+
+# Functions performing each part of the analysis:
+
+retrieve_raw_data <- function (raw_data_dir, force = FALSE) {
+    data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    data_destfile <- "UCI HAR Dataset.zip"
+    if (force || !file.exists(data_destfile)) {
+        download.file(data_url, data_destfile, method = "curl")
+    }
+    if (force || !file.exists(raw_data_dir)) {
+        unzip(data_destfile, overwrite = TRUE)
+        orig_raw_data_dir <-"UCI HAR Dataset"
+        if (raw_data_dir != orig_raw_data_dir) {
+            file.rename(orig_raw_data_dir, raw_data_dir)
+        }
+    }
+}
+
+
+# Call the functions in order:
+
+raw_data_dir <- "UCI HAR Dataset"
+
+retrieve_raw_data(raw_data_dir)
